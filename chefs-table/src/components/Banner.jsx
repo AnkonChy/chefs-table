069ce81a4f7dc0bbd95405bed01c8c3c031f4e60
Ankon Chowdhury <ankonchy1@gmail.com>
2024-10-23 13:19:47 +0600

    banner section completed

--- chefs-table/src/components/Banner.jsx
+++ chefs-table/src/components/Banner.jsx
@@ -1,7 +1,27 @@
+import img from "../assets/banner.jpg";
 const Banner = () => {
   return (
-    <div>
-      <h1>Banner</h1>
+    <div
+      className="bg-cover bg-center rounded "
+      style={{
+        backgroundImage: `url(${img})`,
+        height: "700px",
+      }}
+    >
+      <div className="flex flex-col items-center justify-center text-center text-white px-8 py-32 space-y-10">
+        <h1 className="text-6xl font-bold w-4/6">
+          Discover an exceptional cooking class tailored for you!
+        </h1>
+        <p className="text-lg w-7/12">
+          Learn and Master Basic Programming, Data Structures, Algorithm, OOP,
+          Database and solve 500+ coding problems to become an exceptionally
+          well world-class Programmer.
+        </p>
+        <div className="flex gap-3">
+          <button className="btn">Explore Now</button>
+          <button className="btn">Our Feedback</button>
+        </div>
+      </div>
     </div>
   );
 };
